ae2792bc57876566593ba102054b3097bcdacbfc
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-06-03 00:07:46 +0530

    first commit

--- src/server.ts
+++ src/server.ts
@@ -10,13 +10,26 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "https://admintyppout.vercel.app",
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }));
+
+// app.use(cors({
+//   origin: "https://admintyppout.vercel.app"
+// }));
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://admintyppout.vercel.app",
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+    origin: ["https://admintyppout.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+)
 
 app.use(morgan("combined"));
 app.use(express.json());
